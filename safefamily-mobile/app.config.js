8a7c517faa3b5cae170199e4b2c93ba57249cc03
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-10-04 04:34:12 +0100

    Add Google Maps API key support via env + config

--- safefamily-mobile/app.config.js
+++ safefamily-mobile/app.config.js
@@ -2,19 +2,51 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "SafeFamily",
+    name: "safe-family",
     slug: "safe-family",
+    scheme: "safefamily",
+    version: "1.0.0",
+    extra: {
+      env: "production",
+      router: {},
+      eas: {
+        projectId: "21d023ef-4aa3-4025-a292-fe0f1aeb6366"
+      }
+    },
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
     android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.prayer072.safefamily",
       config: {
         googleMaps: {
           apiKey: process.env.GOOGLE_MAPS_API_KEY
         }
       }
     },
-    ios: {
-      config: {
-        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
-      }
-    }
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      "expo-router"
+    ]
   }
 };
