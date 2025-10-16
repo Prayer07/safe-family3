d18555fa89383ae54942a9fa72cede0c7327cd87
Prayer07 <ezenwaokechukwu3@gmail.com>
2025-10-17 00:25:09 +0100

    omo i tire

--- safefamily-mobile/app.config.js
+++ safefamily-mobile/app.config.js
@@ -14,7 +14,7 @@ export default {
     splash: {
       image: "./assets/icon2.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#be6464ff"
     },
 
     ios: {
@@ -50,7 +50,7 @@ export default {
       [
         "expo-notifications",
         {
-          icon: "./assets/notification-icon.png",
+          // icon: "./assets/notification-icon.png",
           color: "#FF3B30",
           // sounds: ["./assets/sos-alert.wav"]
         }
@@ -66,60 +66,3 @@ export default {
     }
   }
 };
-
-
-
-
-// import 'dotenv/config';
-
-// export default {
-//   expo: {
-//     name: "safe-family",
-//     slug: "safe-family",
-//     scheme: "safefamily",
-//     version: "1.0.0",
-//     extra: {
-//       env: "production",
-//       router: {},
-//       eas: {
-//         projectId: "21d023ef-4aa3-4025-a292-fe0f1aeb6366"
-//       }
-//     },
-//     orientation: "portrait",
-//     icon: "./assets/icon2.png",
-//     userInterfaceStyle: "automatic",
-//     newArchEnabled: true,
-//     splash: {
-//       image: "./assets/icon2.png",
-//       resizeMode: "contain",
-//       backgroundColor: "#ffffff"
-//     },
-//     ios: {
-//       supportsTablet: true,
-//       config: {
-//         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
-//       }
-//     },
-//     android: {
-//       adaptiveIcon: {
-//         foregroundImage: "./assets/icon2.png",
-//         backgroundColor: "#ffffff"
-//       },
-//       edgeToEdgeEnabled: true,
-//       predictiveBackGestureEnabled: false,
-//       package: "com.prayer072.safefamily",
-//       config: {
-//         googleMaps: {
-//           apiKey: process.env.GOOGLE_MAPS_API_KEY
-//         }
-//       }
-//     },
-//     web: {
-//       favicon: "./assets/favicon.png"
-//     },
-//     plugins: [
-//       "expo-router",
-//       // "expo-notifications",
-//     ]
-//   }
-// };
